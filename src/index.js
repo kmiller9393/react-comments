99a28e14d4bc5b321c5676af17f172904f818c7b
kurt miller <kmiller9393@gmail.com>
2019-03-06 19:36:00 -0700

    Refactor comment divs within index.js, into CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM"
+        comment={faker.hacker.phrase()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 5:25PM"
+        comment={faker.hacker.phrase()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 2:15PM"
+        comment={faker.hacker.phrase()}
+      />
     </div>
   );
 };
