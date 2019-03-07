490fd249925bff6291ad85532a7a4682366ef730
kurt miller <kmiller9393@gmail.com>
2019-03-06 20:26:14 -0700

    Wrap each CommentDetail in ApprovalCard to make each ApprovalCard highly reusable

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 4:45PM"
-        comment={faker.hacker.phrase()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 5:25PM"
-        comment={faker.hacker.phrase()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 2:15PM"
-        comment={faker.hacker.phrase()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45PM"
+          comment={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 5:25PM"
+          comment={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 2:15PM"
+          comment={faker.hacker.phrase()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
